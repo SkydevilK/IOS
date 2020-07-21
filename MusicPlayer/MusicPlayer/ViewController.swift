f4cad16f5b3daa14b60bac0cc86f89c80bee7944
skydevilk <skydevilk@gmail.com>
2020-07-21 14:31:55 +0900

    Made Action Function

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var playPauseButton : UIButton!
+    @IBOutlet var timeLabel : UILabel!
+    @IBOutlet var progressSlider : UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("button tapped")
+    }
+    
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("silder value changed")
+    }
 }
-

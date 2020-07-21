a7a2377acd6aca8822bbdf1542e7b2af9bbcb47e
skydevilk <skydevilk@gmail.com>
2020-07-21 13:03:33 +0900

    init

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by kim on 2020/07/21.
+//  Copyright © 2020 Skydevilk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
